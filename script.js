a6e99ccec73aa169c09be331429c2ef5d1e03073
JK083 <jatinsingh320630@gmail.com>
2024-10-09 20:37:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ gsap.to("#page2 h1",{
     scrollTrigger:{
         trigger:"#page2",
         scroller:"body",
-        markers:true,
+        // markers:true,
         scrub:2,
         start:"top 0",
         end:"top -100%",
